3b6ee25a2d7846fd07e1c423ef79db91557b223c
kevingkelly <kevin_kelly@rocketmail.com>
2017-09-01 12:32:48 -0400

    Added Flask test file and initial database schema

---
+++ flaskTest.py
@@ -0,0 +1,54 @@
+from flask import Flask
+
+import sqlite3
+from flask import g
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+DATABASE = '/Users/kevinkelly/Documents/space/AMOS/OpsRE/opsDatabase.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.route('/init_db')
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+        
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+if __name__ == '__main__':
+    app.run()
